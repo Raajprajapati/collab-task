884b4255a39f8b0a9937da68c7ca559910c03264
Suraj Prajapati <raajprajapati60@gmail.com>
2025-12-17 22:25:57 +0530

    fixed dto validation issue

--- backend/src/middleware/validationMiddleware.ts
+++ backend/src/middleware/validationMiddleware.ts
@@ -3,19 +3,23 @@ import { validate, ValidationError } from 'class-validator';
 import { Request, Response, NextFunction } from 'express';
 
 export function validationDto<T extends object>(type: new () => T) {
-    console.log("Errornskdjnkjdn");
     return async (req: Request, res: Response, next: NextFunction) => {
-        // Transform the plain JS object (req.body) to an instance of the DTO class
-        console.log("Req body", req.body);
-        const dtoInstance = plainToInstance(type, req.body, { enableImplicitConversion: true, excludeExtraneousValues: true });
 
-        // Validate the DTO instance
+        if (!req.body) {
+            res.status(400).json({ error: 'No request body provided hehehe' });
+            return;
+        }
+
+        const dtoInstance = plainToInstance(type, req.body, { enableImplicitConversion: true });
+
+        // Validate the DTO instance and return error if any
         const errors: ValidationError[] = await validate(dtoInstance);
 
         if (errors.length > 0) {
             // If there are validation errors, return a 400 Bad Request response
             const errorMessages = errors.map(error => Object.values(error.constraints || {})).flat();
-            return res.status(400).json({ errors: errorMessages });
+            res.status(400).json({ errors: errorMessages });
+            return;
         }
 
         // Attach the validated and transformed DTO instance to the request object
